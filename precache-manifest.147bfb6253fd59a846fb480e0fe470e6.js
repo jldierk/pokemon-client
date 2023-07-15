f6bc8e6876de219bc86d133bfa0b10cbfb4a4845
jldierk <jldierk@gmail.com>
2023-07-15 17:51:17 -0500

    Updates

---
+++ precache-manifest.147bfb6253fd59a846fb480e0fe470e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3abd554d5d93a389b312",
+    "url": "/pokemon-client/static/css/main.6ed6de40.chunk.css"
+  },
+  {
+    "revision": "3abd554d5d93a389b312",
+    "url": "/pokemon-client/static/js/main.3abd554d.chunk.js"
+  },
+  {
+    "revision": "5cc4c16838a79a03d25d",
+    "url": "/pokemon-client/static/js/1.5cc4c168.chunk.js"
+  },
+  {
+    "revision": "f0833bb2a330424b3e99",
+    "url": "/pokemon-client/static/js/2.f0833bb2.chunk.js"
+  },
+  {
+    "revision": "dfbc2296b09a2a6c7701",
+    "url": "/pokemon-client/static/js/runtime~main.dfbc2296.js"
+  },
+  {
+    "revision": "5a9302e4c14cfc61a92ebd0256efbad4",
+    "url": "/pokemon-client/static/media/pokeball.5a9302e4.png"
+  },
+  {
+    "revision": "96fbc110b6b925bbd62cd2845497c266",
+    "url": "/pokemon-client/index.html"
+  }
+];
